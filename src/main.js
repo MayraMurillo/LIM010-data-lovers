554a0bfbaa3699d92fa9425775f0cd30098d962a
MayraMurillo <mmurillor@gmail.com>
2019-07-01 12:43:58 -0500

    subiendo sprint 1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-/* Manejo del DOM */
+// declarando variables //
+const password = document.getElementById('pw');
+const submit = document.getElementById('sbmt');
+const newParagraph = document.getElementById('alert');
+
+
+// asignando contador //
+let counter = 0;
+
+// jalando datos de contraseña //
+submit.addEventListener('click', ()=>{
+  if (password.value === 'LABORATORIA') {
+    alert('hola');
+    counter = 0;
+  } else if (counter < 2) {
+    counter++; 
+    newParagraph.innerHTML = 'Contraseña errada, tenga cuidado. Sólo tiene 3 intentos';
+  } else { 
+    newParagraph.innerHTML = 'ACCESO DENEGADO'; 
+  }
+});
