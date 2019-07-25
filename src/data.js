520bc6ad648a992f1609ceda6eec409277bd10ba
Gabby97 <gabbacvdor@gmail.com>
2019-07-25 16:05:52 -0500

    subiendo avances

--- src/data.js
+++ src/data.js
@@ -1,22 +1,49 @@
-/*const showData = (data) => {
-  let show = '';
-  for (let i=0; i<data.length; i++) {
-    let call = `<li><a href = ""> ${data[i].indicatorName}</a></li>`;
-
-    show += call
-  }
-  return show;
+const paises = (data) => {
+   const arrPaises = [];
+   for (let i = 0; i < data.length; i++) {
+      arrPaises.push(arrDatos[i].countryName);
+   }
+   return arrPaises;
 };
 
+const indicadorPaises = (data, pais) => {
+   let arrIndicadores = [];
+   let indicadores = data[pais].indicators;
+   for (let i = 0; i < indicadores.length; i++) {
+      arrIndicadores.push(indicadores[i]['indicatorName']);
+   }
+   return arrIndicadores;
+};
 
-const information = Object.keys(menuPaises[0].data);
-const porcentaje = Object.values(menuPaises[0].data);
-const nuevaData = () => {
-  let arreglo = [];
-  for (let i=0; i<information.length; i++) {
-  arreglo.push(information[i],porcentaje[i])
-console.log(arreglo);
-return arreglo;
-}
-}
-nuevaData();*/
+window.worldbank = {
+   paises: paises,
+   indicadorPaises: indicadorPaises,
+   // mostrarIndicador: mostrarIndicador,
+};
+//de aquiiii
+// const paises = (data) =>{
+//    const arrPaises = Object.keys(data);
+//    return arrPaises;
+// };
+
+// const indicadorPaises = (data, pais) =>{
+//    let arrIndicadores = [];
+//    let indicadores = data[pais].indicators;
+//    for (let i=0; i<indicadores.length; i++){
+//       arrIndicadores.push(indicadores[i]['countryName']+indicadores[i]['indicatorName']);
+//    }
+//    return arrIndicadores;
+// }
+
+
+// window.worldbank = {
+//    paises: paises,
+//    indicadorPaises: indicadorPaises,
+//    // mostrarData: mostrarData, 
+// };
+// ghasta aquiiii
+
+
+
+
+
